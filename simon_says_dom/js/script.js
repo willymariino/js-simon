62f80213b75398a4e1df5ee5e404043aab4a9052
willymariino <wg.mariino@gmail.com>
2025-03-06 15:56:01 +0100

    added HTML element in javascript

---
+++ simon_says_dom/js/script.js
@@ -0,0 +1,6 @@
+const countdownElement = document.getElementById("countdown");
+const instructions = document.getElementById("instructions");
+const numbersList = document.getElementById("numbers-list");
+const answersForm = document.getElementById("answers-form");
+const inputGroup = document.getElementById("input-group");
+const message = document.getElementById("message");
