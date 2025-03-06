f163bfc7c9c555562d12017e09a38a9839b3832d
willymariino <wg.mariino@gmail.com>
2025-03-06 16:28:31 +0100

    created random number generator

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -4,3 +4,14 @@ const numbersList = document.getElementById("numbers-list");
 const answersForm = document.getElementById("answers-form");
 const inputGroup = document.getElementById("input-group");
 const message = document.getElementById("message");
+
+// quanti numeri generare
+const numberCount = 5
+const timerDuration = 30
+
+// variabili di gioco
+let = RandomNumbers = []
+let timer = TimerDuration
+
+// generatore di numeri casuali
+for (let i = 0; i < numberCount; i++)
