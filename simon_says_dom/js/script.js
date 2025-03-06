30d7c807744f11d3e4ef8f071d3ac2180b8aed17
willymariino <wg.mariino@gmail.com>
2025-03-06 16:50:52 +0100

    generatore di numeri casuali

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -11,6 +11,8 @@ const timerDuration = 30
 
 
 // generatore di numeri casuali
+const randomNumbers = []
 for (let i = 0; i < numberCount; i++) {
     let num = Math.round(Math.random() * 50) + 1
+    randomNumbers.push(num)
 } console.log(num)
